705a1539ef1b4c718bc5a913cc58415c5813c005
PedroPF <pedrop.fernandez95@gmail.com>
2016-04-05 22:25:07 -0300

    Variaveis novas introduzidas, cada ponto tem uma fila, passageiros entram na fila do seu ponto inicial

--- onibus.c
+++ onibus.c
@@ -5,6 +5,74 @@
 #include <semaphore.h>
 #include <time.h>
 
+
+typedef int elem;
+
+typedef struct node {
+	elem e;
+	struct node *prox;
+} node_t;
+
+typedef struct fifo {
+	int size;
+	node_t *first;
+} fifo_t;
+
+void fifo_init(fifo_t *lista){	// Inicializa as variaveis da fila
+	lista->size = 0;
+	lista->first = NULL;
+}
+
+int fifo_insert(elem e, fifo_t *fifo){	// Insere um elemento no fim da fila
+	node_t *new = malloc(sizeof(node_t));
+	if(!new){
+		printf("Nao foi possivel alocar memoria!\n");
+		return 0;
+	}
+	new->e = e;
+	fifo->size++;
+	if(fifo->first == NULL){
+		fifo->first = new;
+	} else {
+		new->prox = fifo->first;
+		fifo->first = new;
+	}
+	return 1;
+}
+
+elem fifo_pop(fifo_t *fifo){	// Remove o primeiro elemento da fila e o retorna
+	elem temp;
+	node_t *temp_node;
+	if(fifo->size == 0){
+		return -1;
+	} else if(fifo->size == 1){
+		temp = fifo->first->e;
+		free(fifo->first);
+		fifo->first = NULL;
+		fifo->size--;
+		return temp;
+	} else {
+		temp_node = fifo->first;
+		while(temp_node->prox->prox){
+			temp_node = temp_node->prox;
+		}
+		temp = temp_node->prox->e;
+		free(temp_node->prox);
+		temp_node->prox = NULL;
+		fifo->size--;
+		return temp;
+	}
+}
+
+void print_fifo(fifo_t fifo){	// Imprime a fila
+	node_t *temp_node = fifo.first;
+	while(temp_node){
+		printf("%d ",temp_node->e);
+		temp_node = temp_node->prox;
+	}
+	printf("\n");
+}
+
 int S, P, C, A; //Número de Paradas(S), Passageiros(P), Carros(C), Assentos(A).
 
 //Vetores para guardar threads  
@@ -13,6 +81,10 @@ pthread_t *pontos;
 pthread_t *passageiros;
 //Vetor dos mutex que garantem que apenas um onibus ocupe um ponto
 pthread_mutex_t *locks_pontos;
+//Vetor com as filas de cada ponto
+fifo_t *filas_passageiro;
+//Vetor com estados de cada passageiro
+char *estados_passageiro;
 
 
 /*As structs abaixo iniciadas com "args_" visam
@@ -21,13 +93,11 @@ pthread_mutex_t *locks_pontos;
 
 typedef struct args_passageiro {
 	int id;                 // PID da thread
-	int inicio;             // Inicio do percurso de cada passageiro
-	int destino;            // Destino final do passageiro 
+	int S;					// Numero de pontos
 } args_passageiro_t;
 
 typedef struct args_carro {
 	int id;                 // PID da thread
-	int A;                  // Numero de assentos
 	int ponto_incial;       // Ponto inicial deste onibus
 } args_carro_t;
 
@@ -41,8 +111,13 @@ typedef struct args_ponto {
 void *passageiro(void *args){
 	args_passageiro_t *temp = (args_passageiro_t *) args;
 	int pid = temp->id;
-	int inicio = temp->inicio;
-	int destino = temp->destino;
+	int inicio = rand() % temp->S;
+	int destino = rand() % temp->S;;
+	while(destino == inicio){
+		destino = rand() % temp->S;
+	}
+	estados_passageiro[pid] = 'w';	// w de waiting, ou seja, passageiro esperando um onibus
+	fifo_insert(pid, filas_passageiro+inicio);
 	//printf("Oi, sou o passageiro numero %d, meu inicio e %d e meu destino, %d\n", pid, inicio, destino);
 	return NULL;
 }
@@ -52,6 +127,7 @@ void *carro(void *args){
 	int pid = temp->id;
 	int ponto_atual = temp->ponto_incial;
 	int ponto_inicial;
+	int ocupantes[A];
 
 	while(pthread_mutex_trylock(locks_pontos+ponto_atual) != 0){
 		if(++ponto_atual >= S){
@@ -68,14 +144,14 @@ void *carro(void *args){
 		ponto_atual =0;
 		}
 	}
-	printf("carro %d:\tinicio:%d,\tfim:%d\n", pid, ponto_inicial, ponto_atual);
+	// printf("carro %d:\tinicio:%d,\tfim:%d\n", pid, ponto_inicial, ponto_atual);
 	return NULL;
 }
 
 void *ponto(void *args){
 	args_ponto_t *temp = (args_ponto_t *) args;
 	int pid = temp->id;
-	pthread_mutex_t *lock = locks_pontos+pid;
+
 	//printf("Oi, sou o ponto numero %d\n", pid);
 	return NULL;
 }
@@ -102,73 +178,6 @@ int *def_pontos_inciais(int num_pontos, int num_onibus){
 	return lista;
 }
 
-typedef int elem;
-
-typedef struct node {
-	elem e;
-	struct node *prox;
-} node_t;
-
-typedef struct fifo {
-	int size;
-	node_t *first;
-} fifo_t;
-
-void fifo_init(fifo_t *lista){
-	lista->size = 0;
-	lista->first = NULL;
-}
-
-int fifo_insert(elem e, fifo_t *fifo){
-	node_t *new = malloc(sizeof(node_t));
-	if(!new){
-		printf("Nao foi possivel alocar memoria!\n");
-		return 0;
-	}
-	new->e = e;
-	fifo->size++;
-	if(fifo->first == NULL){
-		fifo->first = new;
-	} else {
-		new->prox = fifo->first;
-		fifo->first = new;
-	}
-	return 1;
-}
-
-elem fifo_pop(fifo_t *fifo){
-	elem temp;
-	node_t *temp_node;
-	if(fifo->size == 0){
-		return -1;
-	} else if(fifo->size == 1){
-		temp = fifo->first->e;
-		free(fifo->first);
-		fifo->first = NULL;
-		fifo->size--;
-		return temp;
-	} else {
-		temp_node = fifo->first;
-		while(temp_node->prox->prox){
-			temp_node = temp_node->prox;
-		}
-		temp = temp_node->prox->e;
-		free(temp_node->prox);
-		temp_node->prox = NULL;
-		fifo->size--;
-		return temp;
-	}
-}
-
-void print_fifo(fifo_t fifo){
-	node_t *temp_node = fifo.first;
-	while(temp_node){
-		printf("%d ",temp_node->e);
-		temp_node = temp_node->prox;
-	}
-	printf("\n");
-}
-
 int main(int argc,char *argv[]){
 	//Recebe os argumentos do programa e trata erros 
 	if( argc != 5 ){
@@ -184,6 +193,7 @@ int main(int argc,char *argv[]){
 		return 2;
 	}
 	printf("S:%d C:%d P:%d A:%d\n", S, C, P, A);
+
 	//Alocando memoria para threads  
 	carros = calloc(C,sizeof(pthread_t));
 	pontos = calloc(S,sizeof(pthread_t));
@@ -194,21 +204,16 @@ int main(int argc,char *argv[]){
 	args_carro_t* args_carro_init = calloc(C,sizeof(args_carro_t));
 	args_ponto_t* args_ponto_init = calloc(S,sizeof(args_ponto_t));
 	args_passageiro_t* args_passageiro_init = calloc(P,sizeof(args_passageiro_t));
+	//Alocando memoria e inicializando a fila de cada ponto
+	filas_passageiro = calloc(S,sizeof(fifo_t));
+	int i;
+	for(i = 0;i < S;i++){
+		fifo_init(filas_passageiro+i);
+	}
+	//Alocando memoria para o vetor de estados dos passageiros
+	estados_passageiro = calloc(P,sizeof(char));
 
-	int i, temp;
-	int err;
-
-	fifo_t teste;
-	fifo_init(&teste);
-
-	fifo_insert(4, &teste);
-	fifo_insert(7, &teste);
-	fifo_insert(1, &teste);
-
-	printf("%d ", fifo_pop(&teste));
-	printf("%d ", fifo_pop(&teste));
-	printf("%d\n", fifo_pop(&teste));
-
+	int temp, err;
 	
 	//Iniciando threads dos pontos
 	for(i=0;i<S;i++){
@@ -233,7 +238,6 @@ int main(int argc,char *argv[]){
 	for(i=0;i<C;i++){
 		err = 0;
 		args_carro_init[i].id = i;
-		args_carro_init[i].A = A;
 		args_carro_init[i].ponto_incial = pontos_iniciais[i];
 		while(pthread_create(carros+i,NULL,carro,(void*)(args_carro_init+i))){
 			if(err >= 10){
@@ -248,7 +252,7 @@ int main(int argc,char *argv[]){
 	for(i=0;i<P;i++){
 		err = 0;
 		args_passageiro_init[i].id = i;
-		args_passageiro_init[i].inicio = rand() % S;
+		args_passageiro_init[i].S = S;
 
 		do{
 		temp = rand() % S;
@@ -289,6 +293,9 @@ int main(int argc,char *argv[]){
 	free(args_ponto_init);
 
 	free(pontos_iniciais);
+
+	free(filas_passageiro);
+	free(estados_passageiro);
 
 	return 0;
 }
